6dcec6e4b0768dfaf092f5319d94721d625022dd
HeloPras <maharjanprasiddha8@gmail.com>
2026-07-18 00:14:16 +0545

    added message and new message

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,12 @@
 import ChatInterface from "@/components/landing/ChatInterface";
 
 export default function Home() {
-	
+
 	return (
 		<>
-		<div>
-		<ChatInterface></ChatInterface>		
-		</div>
+			<div>
+				<ChatInterface></ChatInterface>
+			</div>
 		</>
 	);
 }
