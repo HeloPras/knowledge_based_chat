32e8053d1a16ffb0b5249219763b08519993bdd3
HeloPras <maharjanprasiddha8@gmail.com>
2026-07-23 23:18:41 +0545

    feat: added api chat streaming using vercel sdk

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,8 +1,28 @@
 import { NextRequest, NextResponse } from "next/server";
+import {
+  streamText,
+  UIMessage,
+  convertToModelMessages,
+  toUIMessageStream,
+  createUIMessageStreamResponse,
+} from "ai";
+import { google } from "@ai-sdk/google";
 
-export function POST(req: NextRequest) {
+export async function POST(req: NextRequest) {
+  const { messages }: { messages: UIMessage[] } = await req.json();
   try {
+    const result = streamText({
+      model: google("gemini-2.5-flash"),
+      prompt: await convertToModelMessages(messages),
+      instructions:
+        "Only provide with text, don't respond with markdown, points, headlines, bulletpoints, only text",
+    });
+
+    return createUIMessageStreamResponse({
+      stream: toUIMessageStream({ stream: result.stream }),
+    });
   } catch (error) {
+    console.error(error);
     return NextResponse.json({ message: "Failed" }, { status: 400 });
   }
 
--- components/landing/ChatInterface.tsx
+++ components/landing/ChatInterface.tsx
@@ -1,39 +1,49 @@
 'use client'
 
-import { useEffect, useState } from "react"
+import { useState } from "react"
+
+import { useChat } from "@ai-sdk/react"
 
 const ChatInterface = () => {
 
-	const [message, setMessage] = useState<string>("")
+	const [input, setInput] = useState<string>('')
+	const { messages, sendMessage } = useChat()
 
 	const submit = async (e: React.FormEvent<HTMLFormElement>) => {
 		e.preventDefault()
 		try {
-			const response = await fetch("api/input", {
-				method: "POST",
-				body: JSON.stringify({ "message": message })
-			})
-			const body = await response.json()
+			sendMessage({ text: input })
+			setInput("")
 
 		} catch (error) {
 			console.log(error)
 		}
 	}
 
-	useEffect(() => {
-		console.log(message)
-	}, [message])
 
 	return (
 		<>
 			<div className=" flex flex-col w-full h-screen bg-[#1f1f1e] justify-end  " >
 				<div className="max-w-7xl h-[80%] mx-auto overflow-y-auto " >
 					This is the converstaion part
+					{messages.map(message => (
+						<div key={message.id}>
+							{message.role == 'user' ? 'User:' : 'AI:'}
+							{message.parts.map((part, i) => {
+								switch (part.type) {
+									case "text":
+										return (<div className="" key={message.id}>
+											{part.text}
+										</div>)
+								}
+							})}
+						</div>
+					))}
 				</div>
 				<div className="place-content-center   mx-auto max-w-1/3 bottom-3 inline-block   ">
 					<div className="bg-[#2c2c2a] rounded-2xl" >
 						<form onSubmit={submit}>
-							<input onChange={(e) => { setMessage(e.currentTarget.value) }} type="text" className=" w-80 h-35 ">
+							<input value={input} onChange={(e) => { setInput(e.currentTarget.value) }} type="text" className=" w-80 h-35 ">
 							</input>
 						</form>
 					</div>
