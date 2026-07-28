255506cf6de5c505809f438810135f3271fd39b8
HeloPras <maharjanprasiddha8@gmail.com>
2026-07-28 23:27:25 +0545

    feat: connected prisma with supabase (success crud)

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -2,15 +2,41 @@ import { NextRequest, NextResponse } from "next/server";
 import {
   streamText,
   UIMessage,
+  UIDataTypes,
+  UITools,
   convertToModelMessages,
   toUIMessageStream,
   createUIMessageStreamResponse,
 } from "ai";
 import { google } from "@ai-sdk/google";
+import { PrismaClient } from "@/app/generated/prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
+
+async function insertUserPrompt(
+  message: UIMessage<unknown, UIDataTypes, UITools>,
+) {
+  const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
+
+  const prisma = new PrismaClient({ adapter });
+  if (!message.parts) {
+    return;
+  }
+  if (message?.parts[0].type != "text") {
+    return;
+  }
+  prisma.message.create({
+    data: {
+      role: "User",
+      content: message.parts[0].text,
+    },
+  });
+}
 
 export async function POST(req: NextRequest) {
   const { messages }: { messages: UIMessage[] } = await req.json();
-  // console.log(messages);
+  insertUserPrompt(messages[messages.length - 1]);
+  // console.log(messages[messages.length - 1]?.parts[0]?.text ?? "");
+
   //
   // console.log(await convertToModelMessages(messages));
   try {
@@ -23,7 +49,8 @@ export async function POST(req: NextRequest) {
     const message = createUIMessageStreamResponse({
       stream: toUIMessageStream({ stream: result.stream }),
     });
-    // console.log(message);
+    // const something = await message.text();
+    // console.log(something);
     return message;
   } catch (error) {
     console.error(error);
---
+++ prisma/migrations/20260728164431_added_new_tables/migration.sql
@@ -0,0 +1,60 @@
+/*
+  Warnings:
+
+  - The primary key for the `User` table will be changed. If it partially fails, the table could be left without primary key constraint.
+  - You are about to drop the column `email` on the `User` table. All the data in the column will be lost.
+  - You are about to drop the column `name` on the `User` table. All the data in the column will be lost.
+  - You are about to drop the `Post` table. If the table is not empty, all the data it contains will be lost.
+  - Added the required column `password` to the `User` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `userName` to the `User` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- CreateEnum
+CREATE TYPE "Role" AS ENUM ('AI', 'User', 'System');
+
+-- DropForeignKey
+ALTER TABLE "Post" DROP CONSTRAINT "Post_authorId_fkey";
+
+-- DropIndex
+DROP INDEX "User_email_key";
+
+-- AlterTable
+ALTER TABLE "User" DROP CONSTRAINT "User_pkey",
+DROP COLUMN "email",
+DROP COLUMN "name",
+ADD COLUMN     "password" TEXT NOT NULL,
+ADD COLUMN     "userName" TEXT NOT NULL,
+ALTER COLUMN "id" DROP DEFAULT,
+ALTER COLUMN "id" SET DATA TYPE TEXT,
+ADD CONSTRAINT "User_pkey" PRIMARY KEY ("id");
+DROP SEQUENCE "User_id_seq";
+
+-- DropTable
+DROP TABLE "Post";
+
+-- CreateTable
+CREATE TABLE "Conversation" (
+    "id" SERIAL NOT NULL,
+    "userId" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Conversation_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Message" (
+    "id" SERIAL NOT NULL,
+    "conversationId" INTEGER NOT NULL,
+    "role" "Role" NOT NULL DEFAULT 'System',
+    "content" TEXT NOT NULL,
+
+    CONSTRAINT "Message_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "Conversation" ADD CONSTRAINT "Conversation_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Message" ADD CONSTRAINT "Message_conversationId_fkey" FOREIGN KEY ("conversationId") REFERENCES "Conversation"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
