c5b3c233e3d8bca7ccb560474e03467d8ddb5569
HeloPras <maharjanprasiddha8@gmail.com>
2026-08-25 23:04:59 +0545

    feat: logic to upload file under same UUID

--- app/api/chat/uploadFile/route.ts
+++ app/api/chat/uploadFile/route.ts
@@ -1,20 +1,47 @@
+import { auth } from "@/lib/auth/auth";
 import { supabase } from "@/lib/supabase/client";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(req: NextRequest) {
+  const user = await auth();
+
+  console.log(user?.user?.id);
+
   const body = await req.formData();
   const file = body.get("file");
 
   if (!file) {
     return NextResponse.json({ error: "no file uploaded" }, { status: 400 });
   }
-  try {
-    const { data, error } = await supabase.storage
-      .from("knowledge_base_file")
-      .upload("file_path", file);
 
-    if (error) throw Error(error.message);
-    return NextResponse.json({ data: data }, { status: 200 });
+  try {
+    if (!user?.user?.id) {
+      throw Error("User is not authenticated");
+    }
+
+    const bucketAvailable = await supabase.storage.getBucket(user.user.id);
+
+    if (bucketAvailable.error) {
+      throw Error(bucketAvailable.error.message);
+    }
+
+    if (!bucketAvailable.data) {
+      const { error } = await supabase.storage.createBucket(user.user.id);
+      if (error) {
+        throw Error(error.message);
+      }
+    }
+
+    const uploadedData = await supabase.storage
+      .from("knowledge_base_file")
+      .upload(user.user.id, file);
+
+    if (uploadedData.error) {
+      throw Error(uploadedData.error.message);
+    }
+    console.log(uploadedData);
+
+    return NextResponse.json({ data: uploadedData.data }, { status: 200 });
   } catch (error) {
     return NextResponse.json(
       { error: `Faced Issues while uploading ${error}` },
